a0acb7e0389995d12ac5b043c674f68245243cd8
sandra629 <sandra.demitseva@gmail.com>
2015-07-09 17:40:30 +0300

    sth

---
+++ Application Layer/Areas/Account/AccountAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace ApplicationLayer.Areas.Account
+{
+    public class AccountAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Account";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+        "Account_default",
+        "Account/{controller}/{action}/{id}",
+        new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+        new string[] { "Login.Areas.Account.Contollers" }
+            );
+        }
+    }
+}
---
+++ Application Layer/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using ApplicationLayer.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+
+namespace ApplicationLayer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
